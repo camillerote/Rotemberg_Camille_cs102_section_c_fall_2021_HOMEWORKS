fbce025d7682a35f550c07112a61f43706cfb1b9
YOUR_FIRST_NAME YOUR_LAST_NAME <YOUR@EMAIL_ADDRESS>
2021-09-29 23:03:37 +0000

    finish mean

---
+++ Makefile
@@ -0,0 +1,7 @@
+runpart3:
+	gcc part3.c
+	./a.out
+
+runpart4:
+	gcc part4.c
+	./a.out
---
+++ part3.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main() {
+    int num1;
+    printf("Enter first number: ");   
+    scanf("%d",&num1);
+  
+    int num2;
+    printf("Enter first number: ");
+    scanf("%d",&num2);
+    
+    int num3;
+    printf("Enter first number: ");
+    scanf("%d",&num3);
+    
+    int num4;
+    printf("Enter first number: ");
+    scanf("%d",&num4);
+    
+    int num5;
+    printf("Enter first number: ");
+    scanf("%d",&num5);
+    
+    printf("Mean = %f ", (num1+num2+num3+num4+num5)/5.0);
+}
+
+
