79cc2a2f5146ec71a4af7cb59ed791e155cd5424
YOUR_FIRST_NAME YOUR_LAST_NAME <YOUR@EMAIL_ADDRESS>
2021-10-02 15:26:34 +0000

    ASCII string

---
+++ part5.c
@@ -0,0 +1,620 @@
+#include <stdio.h>
+
+void A(int row) { 
+    switch(row)
+    {
+        case 0:
+                printf(" # "); 
+            break;
+        case 1:
+                printf(" # # "); 
+            break;
+        case 2: 
+                printf(" ### ");
+            break;
+        case 3: 
+                printf(" #  # ");
+            break;
+        case 4:
+                printf(" #   # ");
+        default:
+            break;
+    }
+}
+void B(int row) { 
+    switch(row)
+    {
+        case 0:
+                printf("####"); 
+            break;
+        case 1:
+                printf("#  #"); 
+            break;
+        case 2: 
+                printf("####");
+            break;
+        case 3:
+                printf("#  #");
+            break;
+        case 4: printf("####");
+            break;
+        default:
+            break;
+    }
+}
+
+void C(int row) {
+        switch(row)
+      {
+          case 0:
+                  printf("####");
+              break;
+          case 1:
+                  printf("#   ");
+              break;
+          case 2:
+                  printf("#   ");
+              break;
+          case 3:
+                  printf("#   ");
+              break;
+          case 4: printf("####");
+              break;
+          default:
+              break;
+     }
+  }
+
+void D(int row) {
+      switch(row)
+      {
+          case 0:
+                  printf("###");
+              break;
+          case 1:
+                  printf("#  #");
+              break;
+          case 2:
+                  printf("#   #");
+              break;
+          case 3:
+                  printf("#  #");
+              break;
+          case 4: printf("####");
+              break;
+          default:
+              break;
+     }
+  }
+
+
+void E(int row) {
+      switch(row)
+      {
+          case 0:
+                  printf("####");
+              break;
+          case 1:
+                  printf("#   ");
+              break;
+          case 2:
+                  printf("####");
+              break;
+          case 3:
+                  printf("#   ");
+              break;
+          case 4: printf("####");
+              break;
+          default:
+              break;
+     }
+  }
+void F(int row) {
+      switch(row)
+      {
+          case 0:
+                  printf("####");
+              break;
+          case 1:
+                  printf("#   ");
+              break;
+          case 2:
+                  printf("####");
+              break;
+          case 3:
+                  printf("#   ");
+              break;
+          case 4: printf("#   ");
+              break;
+          default:
+              break;
+     }
+  }
+void G(int row) {
+      switch(row)
+      {
+          case 0:
+                  printf("####");
+              break;
+          case 1:
+                  printf("#  ");
+              break;
+          case 2:
+                  printf("# ##");
+              break;
+          case 3:
+                  printf("#  #");
+              break;
+          case 4: printf("####");
+              break;
+          default:
+              break;
+     }
+  }
+
+void H(int row) {
+      switch(row)
+      {
+          case 0:
+                  printf("#  #");
+              break;
+          case 1:
+                  printf("#  #");
+              break;
+          case 2:
+                  printf("####");
+              break;
+          case 3:
+                  printf("#  #");
+              break;
+          case 4: printf("#  #");
+              break;
+          default:
+              break;
+     }
+  }
+
+
+void I(int row) {
+      switch(row)
+      {
+          case 0:
+                  printf("####");
+              break;
+          case 1:
+                  printf(" # ");
+              break;
+          case 2:
+                  printf(" # ");
+              break;
+          case 3:
+                  printf(" # ");
+              break;
+          case 4: printf("####");
+              break;
+          default:
+              break;
+     }
+  }
+
+void J(int row) {
+      switch(row)
+      {
+          case 0:
+                  printf("####");
+              break;
+          case 1:
+                  printf(" #  ");
+              break;
+          case 2:
+                  printf(" #  ");
+              break;
+          case 3:
+                  printf(" #  ");
+              break;
+          case 4: printf(" ###");
+              break;
+          default:
+              break;
+     }
+  }
+
+void K(int row) {
+      switch(row)
+      {
+          case 0:
+                  printf("#  #");
+              break;
+          case 1:
+                  printf("# # ");
+              break;
+          case 2:
+                  printf("#   ");
+              break;
+          case 3:
+                  printf("# # ");
+              break;
+          case 4: printf("#  #");
+              break;
+          default:
+              break;
+     }
+  }void L(int row) {
+      switch(row)
+      {
+          case 0:
+                  printf("#   ");
+              break;
+          case 1:
+                  printf("#   ");
+              break;
+          case 2:
+                  printf("#   ");
+              break;
+          case 3:
+                  printf("#   ");
+              break;
+          case 4: printf("####");
+              break;
+          default:
+              break;
+     }
+  }
+
+void M(int row) {
+      switch(row)
+      {
+          case 0:
+                  printf("#   #");
+              break;
+          case 1:
+                  printf("# # #");
+              break;
+          case 2:
+                  printf("# # #");
+              break;
+          case 3:
+                  printf("#   #");
+              break;
+          case 4: printf("#   #");
+              break;
+          default:
+              break;
+     }
+  }
+
+void N(int row) {
+      switch(row)
+      {
+          case 0:
+                  printf("#     #");
+              break;
+          case 1:
+                  printf("#  #  #");
+              break;
+          case 2:
+                  printf("#   # #");
+              break;
+          case 3:
+                  printf("#    ##");
+              break;
+          case 4: printf("#     #");
+              break;
+          default:
+              break;
+     }
+  }
+void O(int row) {
+      switch(row)
+      {
+          case 0:
+                  printf("####");
+              break;
+          case 1:
+                  printf("#  #");
+              break;
+          case 2:
+                  printf("#  #");
+              break;
+          case 3:
+                  printf("#  #");
+              break;
+          case 4: printf("####");
+              break;
+          default:
+              break;
+     }
+  }
+
+void  P(int row) {
+      switch(row)
+      {
+          case 0:
+                  printf("####");
+              break;
+          case 1:
+                  printf("#  #");
+              break;
+          case 2:
+                  printf("####");
+              break;
+          case 3:
+                  printf("#   ");
+              break;
+          case 4: printf("#   ");
+              break;
+          default:
+              break;
+     }
+  }
+void  Q(int row) {
+      switch(row)
+      {
+          case 0:
+                  printf(" ##   ");
+              break;
+          case 1:
+                  printf("#  #  ");
+              break;
+          case 2:  
+                  printf("#   # ");
+              break;
+          case 3:
+                  printf("#  # #");
+              break;
+          case 4: printf("##### ");
+              break;
+          default:
+              break;
+     }
+  }
+
+void  R(int row) {
+      switch(row)
+      {
+          case 0:
+                  printf("####");
+              break;
+          case 1:
+                  printf("#  #");
+              break;
+          case 2:
+                  printf("####");
+              break;
+          case 3:
+                  printf("#  # ");
+              break;
+          case 4: printf("#   #");
+              break;
+          default:
+              break;
+     }
+  }
+void  S(int row) {
+      switch(row)
+      {
+          case 0:
+                  printf("####");
+              break;
+          case 1:
+                  printf("#  ");
+              break;
+          case 2:
+                  printf(" # ");
+              break;
+          case 3:
+                  printf("#  #");
+              break;
+          case 4: printf("####");
+              break;
+          default:
+              break;
+     }
+  }
+void T(int row) {
+      switch(row)
+      {
+          case 0:
+                  printf("####");
+              break;
+          case 1:
+                  printf("  #  ");
+              break;
+          case 2:
+                  printf("  #  ");
+              break;
+          case 3:
+                  printf("  #  ");
+              break;
+          case 4: printf("  #  ");
+              break;
+          default:
+              break;
+     }
+  }
+
+void U(int row) {
+      switch(row)
+      {
+          case 0:
+                  printf("#  #");
+              break;
+          case 1:
+                  printf("#  #");
+              break;
+          case 2:
+                  printf("#  #");
+              break;
+          case 3:
+                  printf("#  #");
+              break; 
+          case 4: printf("####");
+              break;
+          default:
+              break;
+     }
+  }
+
+void V(int row) {
+      switch(row)
+      {
+          case 0:
+                  printf("#      #");
+              break;
+          case 1:
+                  printf(" #    # ");
+              break;
+          case 2:
+                  printf("  #  #  ");
+              break;
+          case 3:
+                  printf("  # #   ");
+              break;
+          case 4: printf("   #    ");
+              break;
+          default:
+              break;
+     }
+  }
+
+void W(int row) {
+      switch(row)
+      {
+          case 0:
+                  printf("#     #");
+              break;
+          case 1:
+                  printf("##   ##");
+              break;
+          case 2:
+                  printf("#  #  #");
+              break;
+          case 3:
+                  printf("#  #  #");
+              break;
+          case 4: printf("#######");
+              break;
+          default:
+              break;
+     }
+  }
+
+void X(int row) {
+      switch(row)
+      {
+          case 0:
+                  printf("#    #");
+              break;
+          case 1:
+                  printf(" #  # ");
+              break;
+          case 2:
+                  printf("   #  ");
+              break;
+          case 3:
+                  printf(" #  # ");
+              break;
+          case 4: printf("#    #");
+              break;
+          default:
+              break;
+     }
+  }
+
+
+void Y(int row) {
+      switch(row)
+      {
+          case 0:
+                  printf("#   #");
+              break;
+          case 1:
+                  printf("#   #");
+              break;
+
+          case 2:
+                  printf("  #  ");
+              break;
+          case 3:
+                  printf("  #  ");
+              break;
+          case 4: printf("  #  ");
+              break;
+          default:
+              break;
+     }
+  }
+
+
+void Z(int row) {
+      switch(row)
+      {
+          case 0:
+                  printf("####");
+              break;
+          case 1:
+                  printf("  # ");
+              break;
+          case 2:
+                  printf(" #  ");
+              break;
+          case 3:
+                  printf("#   ");
+              break;
+          case 4: printf("####");
+              break;
+          default:
+              break;
+     }
+  }
+
+int main()
+{
+    void (*lPtr[25])(int); //create an array of function pointers?
+        lPtr[0] = &A; //pointer to function A
+        lPtr[1] = &B; //pointer to function B
+        lPtr[2] = &C; //pointer to function C
+        lPtr[3] = &D; //pointer to function D
+        lPtr[4] = &E; //pointer to function E
+        lPtr[5] = &F; //pointer to function F
+        lPtr[6] = &G; //pointer to function G
+        lPtr[7] = &H; //pointer to function H
+        lPtr[8] = &I; //pointer to function I
+        lPtr[9] = &J; //pointer to function J
+        lPtr[10] = &K; //pointer to function K
+        lPtr[11] = &L; //pointer to function L
+        lPtr[12] = &M; //pointer to function M
+        lPtr[13] = &N; //pointer to function N
+        lPtr[14] = &O; //pointer to function O
+        lPtr[15] = &P; //pointer to function P
+        lPtr[16] = &Q; //pointer to function Q
+        lPtr[17] = &R; //pointer to function R
+        lPtr[18] = &S; //pointer to function S
+        lPtr[19] = &T; //pointer to function T
+        lPtr[20] = &U; //pointer to function U
+        lPtr[21] = &V; //pointer to function V
+        lPtr[22] = &W; //pointer to function W
+        lPtr[23] = &X; //pointer to function X
+        lPtr[24] = &Y; //pointer to function Y
+        lPtr[25] = &Z; //pointer to function Z
+
+
+    char nameString[100];
+    scanf("%s", nameString);
+    printf("You entered %s\n", nameString);
+    int i=0;
+    while(nameString[i]){
+            for(int row = 0;  row<5; row++) //rows
+            {
+                for(int character = 0;  character<26; character++){ //get lPtr of each row
+                    lPtr[character](row);
+                    printf("  ");
+                }
+                printf("\n"); 
+        }
+        i++;
+    }
+
+    return 0;
+}
+
