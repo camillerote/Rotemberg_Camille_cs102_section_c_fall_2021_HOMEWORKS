af9940270a864aff931f177ca97c8a072ad8fa02
YOUR_FIRST_NAME YOUR_LAST_NAME <YOUR@EMAIL_ADDRESS>
2021-10-05 18:17:48 +0000

    length and alpha strings

--- part4.c
+++ part4.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-//#define inputted_array 3  //size of the array
+#define inputted_array 3  //size of the array to be able to use it in functions outside main
  
 //function to display array/then print out the inputted  array
 void display(char array[][40]){
@@ -13,58 +13,53 @@ void display(char array[][40]){
 
 void stringLength(char array[][40]){
   char temp[40];
-  // Buuble Sort
+  // Buuble Sort using strlen in stdlib.h
   for(int i=0; i<inputted_array; i++){
     for(int j=0; j<inputted_array-1-i; j++){
-      if(strlen(array[j]) > strlen(array[j+1])){
-        //swap array[j] and array[j+1]
-        strcpy(temp, array[j]);
+      if(strlen(array[j]) > strlen(array[j+1])){       
         strcpy(array[j], array[j+1]);
         strcpy(array[j+1], temp);
       }
     }
   }
-  //display the sorted array
+
   printf("Length Sorted Array: ");
   display(array);
 }
 
-void alpha(char array[][40]){
+void stringAlpha(char array[][40]){
   char temp[40];
-  // Buuble Sort 
+  // Buuble Sort using strcmp to sort arrays  
   for(int i=0; i<inputted_array; i++){
     for(int j=0; j<inputted_array-1-i; j++){
       if(strcmp(array[j], array[j+1]) > 0){
-        //swap array[j] and array[j+1]
-        strcpy(temp, array[j]);
         strcpy(array[j], array[j+1]);
         strcpy(array[j+1], temp);
       }
     }
   }
-  //display the sorted array
+  //stdout sorted sorted array
   printf("Alpha Sorted Array: ");
   display(array);
 }
 
 int main()
 {
-  
-//create an array of strings
+
   char array[inputted_array][40];
 
-  //Inputting names
+  
   printf("Enter %d Lower case letter strings: \n", inputted_array);
   for(int i=0; i<inputted_array; i++){
     scanf("%s", array[i]);
   }
 
-  //display the original array
+  
   printf("Original array: ");
   
   display(array); // printing the array
-  alpha(array); // throwing the array in and sorting it by alphabetical order
-  stringLength(array); // throwing in the array and sorting it by string length  
+  stringAlpha(array); //sorting array alphabetical order
+  stringLength(array); // sorting it by string length  
 
     return 0;
  }
