7a15e47e302d3b105e8ec6f20c5270158486e20a
YOUR_FIRST_NAME YOUR_LAST_NAME <YOUR@EMAIL_ADDRESS>
2021-10-10 17:53:05 +0000

    floats and char

---
+++ HW2/3/myCode.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+    float number = -3.14;
+    printf("The number is: %d\n", number);
+
+    int intNumber = 3;
+    printf("The int number is: %f\n", intNumber);
+
+    char aChar = 'a';
+    
+    printf("The char number is: %d\n", aChar);
+    return 0;
+}
+
+
